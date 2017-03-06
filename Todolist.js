59a95da4c38647f6fb933e9999408887d756c0af
MrZ33 <dhaifallah.zerhoui@gmail.com>
2017-03-06 13:20:13 +0100

    2nd commit

---
+++ Todolist.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+function Toggle() {
+    var x = document.getElementById('week1');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
